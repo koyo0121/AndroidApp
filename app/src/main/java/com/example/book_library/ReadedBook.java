ffa757797e089f73254e981a4bae2c9a6a2a2fd3
yamaki417 <58658711+yamaki417@users.noreply.github.com>
2020-04-04 00:42:05 +0900

    Gird view修正
    This reverts commit 9aef2e45ec7e3c765ca13c0c559830005a4bd8d1.

--- app/src/main/java/com/example/book_library/ReadedBook.java
+++ app/src/main/java/com/example/book_library/ReadedBook.java
@@ -4,16 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
+
 public class ReadedBook extends Fragment {
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
@@ -22,13 +23,27 @@ public class ReadedBook extends Fragment {
         // 表示する画面IDを設定
         View view = inflater.inflate(R.layout.fragment_readed_book,container,false);
 
-        // fragment_readed_bookのimageView2にic_launcher_foregroundを設定
-        ImageView image1 = (ImageView)view.findViewById(R.id.imageView2);
-        image1.setImageResource(R.drawable.ic_launcher_foreground);
+        GridView gridView = (GridView)view.findViewById(R.id.gridView);
+        ArrayList<String> list = new ArrayList<String>();
 
-        // fragment_readed_bookのimageView2にic_launcher_foregroundを設定
-        ImageView image2 = (ImageView)view.findViewById(R.id.imageView3);
-        image2.setImageResource(R.drawable.ic_launcher_foreground);
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+        list.add("G");
+        list.add("H");
+        list.add("I");
+        list.add("J");
+        list.add("K");
+        list.add("L");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, list);
+
+        gridView.setAdapter(adapter);
+
 
         return view;
     }
