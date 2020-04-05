49cc497b1fdcf4ee95007ed06341543897f399be
yamaki417 <58658711+yamaki417@users.noreply.github.com>
2020-04-05 18:36:32 +0900

    Gird view修正
    This reverts commit 9aef2e45ec7e3c765ca13c0c559830005a4bd8d1.

--- app/src/main/java/com/example/book_library/ReadedBook.java
+++ app/src/main/java/com/example/book_library/ReadedBook.java
@@ -1,12 +1,16 @@
 package com.example.book_library;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,29 +19,25 @@ import androidx.fragment.app.Fragment;
 
 
 public class ReadedBook extends Fragment {
+    private String filePath;
+
+    private GridView gridView;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
 
         DatabaseHelper helper = new DatabaseHelper(getActivity());
 
         // 表示する画面IDを設定
         View view = inflater.inflate(R.layout.fragment_readed_book,container,false);
-
         GridView gridView = (GridView)view.findViewById(R.id.gridView);
         ArrayList<String> list = new ArrayList<String>();
 
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.add("D");
-        list.add("E");
-        list.add("F");
-        list.add("G");
-        list.add("H");
-        list.add("I");
-        list.add("J");
-        list.add("K");
-        list.add("L");
+        for(int i = 1; i< 42; i++){
+
+            list.add("TEST"+i);
+        }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, list);
--- app/src/main/java/com/example/book_library/TundokuBook.java
+++ app/src/main/java/com/example/book_library/TundokuBook.java
@@ -4,15 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class TundokuBook extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tundoku_book,container,false);
+        DatabaseHelper helper = new DatabaseHelper(getActivity());
+
+        // 表示する画面IDを設定
+        View view = inflater.inflate(R.layout.fragment_tundoku_book,container,false);
+        GridView gridView = (GridView)view.findViewById(R.id.gridView2);
+        ArrayList<String> list = new ArrayList<String>();
+
+        for(int i = 1; i< 42; i++){
+
+            list.add("TEST"+i);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, list);
+
+        gridView.setAdapter(adapter);
+
+
+        return view;
     }
 }
